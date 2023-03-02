8172feab3dd6fc444dc4624823ecedbd860e8a21
beingakshaykolhe <126570553+beingakshaykolhe@users.noreply.github.com>
2023-03-02 01:15:43 -0800

    Add files via upload

---
+++ Practicals/TestEncapsulation.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class TestEncapsulation 
+{
+	public static void main(String[] args) 
+	{
+		// Creating object of class EncapsulationExample
+		EncapsulationExample a= new EncapsulationExample();
+		//Object is used to access the methods of a class
+		a.setId(1);
+		a.setName("Akshay");
+		System.out.println("Id:"+a.getId()+"\n"+"Name:"+a.getName());
+	}
+}
